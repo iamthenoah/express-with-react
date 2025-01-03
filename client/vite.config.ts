68c2e63f7eab456e86ef8f0cd620caca68685df4
noahgreff <greffnoah@gmail.com>
2025-01-03 10:41:24 +0100

    removed `vite-tsconfig-paths`

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import reactPlugin from '@vitejs/plugin-react'
 
 export default defineConfig({
-	plugins: [tsconfigPaths()],
+	plugins: [reactPlugin()],
 	root: './public',
 	build: {
 		outDir: '../dist',
